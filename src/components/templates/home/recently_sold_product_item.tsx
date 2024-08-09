a6788b1528633ea3b292bef4fc154beb6234b541
Md. Siam Shekh <siam62349@gmail.com>
2024-08-09 14:48:37 +0600

    Sign up page design

--- src/components/templates/home/recently_sold_product_item.tsx
+++ src/components/templates/home/recently_sold_product_item.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Recently_sold_product_item = () => {
     const [onHover, setHover] = useState(false);
     return (
-        <div className="p-[1px] shadow-2xl bg-gradient-to-b from-transparent to-blue-800 rounded-xl">
+        <div className="pt-[1px] pl-[1px] pb-[3px] pr-[3px] shadow-2xl bg-gradient-to-b from-transparent to-blue-800 rounded-xl">
             <div className="bg-white  rounded-xl relative h-64 w-full" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 <div className="absolute top-0 w-full rounded-t-xl h-40">
                     <img src="https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Sobuj_Dhaka_Garden_Dahao_Cockroach_Kille-Sobuj_Dhaka_Garden_Shop-38565-418166.jpg" alt="products" className={`rounded-t-xl w-full h-40 absolute ${onHover && 'opacity-30'}`} />
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,8 +1,42 @@
+import bg_image from "../assets/images/sign up/kids_reading_book_animated.webp";
+import logo from "../assets/icons/icon.png";
+import { FaApple, FaGithub, FaGoogle } from "react-icons/fa";
+
 
 const SignUp = () => {
     return (
-        <div>
-            
+        <div data-theme="light" className="h-screen w-full relative flex justify-between items-center">
+            <div className="flex-[2] hidden md:block">
+                <img src={bg_image} alt="background image" className="object-cover h-screen" draggable={false} />
+            </div>
+            <div className="bg-white flex-1 flex justify-center items-center flex-col gap-3 ">
+                <img src={logo} alt="" />
+                <h3 className="capitalize font-teko text-3xl mt-10">Create an account</h3>
+
+                <div className="flex items-center w-72 lg:w-80 px-10 py-2 border-black border-t border-l border-b-2 border-r-2 capitalize font-genos font-bold cursor-pointer hover:bg-black hover:text-white justify-center gap-3">
+                    <FaGoogle className="size-6" />
+                    <p>continue with google</p>
+                </div>
+
+                <div className="flex items-center w-72 lg:w-80 px-10 py-2 border-black border-t border-l border-b-2 border-r-2 capitalize font-genos font-bold cursor-pointer hover:bg-black hover:text-white justify-center gap-3">
+                    <FaApple className="size-6" />
+                    <p>continue with apple</p>
+                </div>
+
+                <div className="flex items-center w-72 lg:w-80 px-10 py-2 border-black border-t border-l border-b-2 border-r-2 capitalize font-genos font-bold cursor-pointer hover:bg-black hover:text-white justify-center gap-3">
+                    <FaGithub className="size-6" />
+                    <p>continue with github</p>
+                </div>
+
+                <p className="w-80 text-center font-poppins text-black text-opacity-60">
+                    By continuing, you agree to Freepik Company’s <a href="/#">Terms of Use</a> and <a href="/#">Privacy Policy</a>.
+                </p>
+
+                <p className="w-80 text-center font-poppins font-bold text-black">
+                Already have an account? <a href="/#" className="text-blue-600">Log in</a>
+                </p>
+
+            </div>
         </div>
     );
 };
